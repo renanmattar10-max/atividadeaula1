17620639b4c157009cfdff0d52322950ccfdadb6
renanmattar10-max <renanmattar10@gmail.com>
2026-08-28 22:46:59 -0300

    Correçao questao 7

--- src/questao7.php
+++ src/questao7.php
@@ -1,8 +1,10 @@
 <?php
 
+$nome = (string) readline("Digite o nome do produto: ");
 $preco = (float) readline("Digite o preço do produto: ");
 $quantidade = (int) readline("Digite a quantidade de produtos: ");
 
-$total = $preco + $quantidade;
+$total = $preco * $quantidade;
 
+echo "Nome do produto: " . $nome. "\n";
 echo "Valor total da compra: R$" . $total . "\n";
